46d5a90f74f334bd7d13cafbcdb23f986ab4ff3e
Chan-Man-Lock-Jake <jakechan.ml@gmail.com>
2024-05-01 15:32:57 +0800

    Update to LinkedIn learning

--- LinkedIn/express-essentials/index.js
+++ LinkedIn/express-essentials/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import data from "./data/MOCK_DATA.json" assert { type: "json" };
 
 const app = express();
@@ -8,10 +8,26 @@ app.use(express.static('public'));
 
 app.use('/images', express.static('images'));
 
-app.get('/', (request, response) => {
-    response.json(data);
-    //response.send('This is a GET request at /')
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true }));
+
+app.post('/item', (request, response) => {
+    console.log(request.body);
+    response.send(request.body); 
 });
+
+app.route('/class')
+.get((request, response) => {
+    //response.send('Retrieve class info');
+    throw new Error();
+})
+.post((request, response) => {
+    response.send('Create class info');
+})
+.put((request, response) => {
+    response.send('Update class info');
+})
 
 //GET
 app.get('/class/:id', (request, response) => {
@@ -20,6 +36,28 @@ app.get('/class/:id', (request, response) => {
     response.send(student);
     
     //console.log(request.params);
+});
+
+app.get('/', (request, response) => {
+    response.json(data);
+    //response.send('This is a GET request at /')
+});
+
+app.get('/next',(request, response, next) => {
+    console.log('The response will be sent by the next function.');
+    next();
+},
+(request, response) => {
+    response.send('I just set up a route with a second callback.');
+}
+);
+
+app.get('/download', (request, response) => {
+response.download('images/mountains_2.jpeg');
+});
+
+app.get('/redirect', (request, response) => {
+response.redirect('http://www.linkedin.com');
 });
 
 //CREATE - POST saveguards input with encryption.
@@ -37,9 +75,14 @@ app.delete('/delete', (request, response) => {
     response.send('This is a DELETE request at /delete')
 });
 
-app.listen(PORT, () => {
-    console.log(`The server is running on ${PORT}`);
-    console.log(data);
+app.use((err, req, res, next) =>
+{
+    console.error(err.stack);
+    res.status(500).send('Something is broken!');
 });
 
-//
+app.listen(PORT, () => {
+    console.log(`The server is running on ${PORT}`);
+});
+
+//set DEBUG=express:* node --experimental-json-modules index.js
